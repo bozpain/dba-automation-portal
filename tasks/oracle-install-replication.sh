0e1832baebdef54a831e04375a36a0adce97ac0f
bozpain <bozpain@gmail.com>
2026-05-17 20:25:14 +0700

    update

--- tasks/oracle-install-replication.sh
+++ tasks/oracle-install-replication.sh
@@ -7,6 +7,7 @@ parse_survey_args "$@"
 
 PROJECT_ROOT="${ORACLE_INSTALL_REPLICATION_ROOT:-/dbaportal/automation/projects/oracle-install-replication-framework}"
 PYTHON_BIN="${PYTHON_BIN:-python3.12}"
+export ORACLE_AUTO_REPORT_PUBLISH_PATH="${ORACLE_AUTO_REPORT_PUBLISH_PATH:-${DBA_PORTAL_ROOT:-/dbaportal}/exports/install-reports}"
 
 ACTION="$(get_arg ACTION validate-config)"
 CONFIG="$(get_arg CONFIG configs/gcp-single-gi-lab.json)"
